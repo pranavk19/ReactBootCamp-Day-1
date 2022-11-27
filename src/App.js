96fac3cbc8eecb6d853ec74f21fa6a5a72d63eb4
pranavk19 <kumarpranav902@gmail.com>
2022-11-27 18:28:41 +0530

    BootCamp Day 2 Changes

---
+++ src/App.js
@@ -0,0 +1,6 @@
+const heading = React.createElement(
+    "h1",
+    {id : "title"},
+    "hello world");
+ const root = ReactDOM.createRoot(document.getElementById("root"));
+ root.render(heading);
